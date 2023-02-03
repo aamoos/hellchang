97efe9168655d46a56db1ec5bc3a17b4bb498490
aamoos <aamoos@naver.com>
2023-02-03 14:14:09 +0900

    Initial commit

---
+++ src/test/java/com/hellchang/HellchangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellchang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellchangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
