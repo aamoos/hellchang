5c3b95eeebf73dcc78de5dc075a17859ea252eb8
hhyyyjun <jun950617@naver.com>
2023-05-04 16:46:21 +0900

    api단 유효성 검사 추가

--- src/main/java/com/hellchang/dto/UserDto.java
+++ src/main/java/com/hellchang/dto/UserDto.java
@@ -1,9 +1,10 @@
 package com.hellchang.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
+import lombok.Data;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 /**
@@ -13,35 +14,31 @@ import javax.validation.constraints.Size;
 * @author : hj
 * @Description: 회원가입 시 사용
 **/
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class UserDto {
 
     @NotNull
-    @Size(min = 3, max = 50)
+    @Size(max = 50)
     private String userid;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @NotNull
-    @Size(min = 3, max = 100)
+    @NotNull(message = "비밀번호는 필수입력 값입니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[a-zA-Z\\d!@#$%^&*]{8,14}$", message = "비밀번호는 8~14글자 사이며 영어, 숫자, 특수문자 필수입니다.")
     private String password;
 
-    @NotNull
-    @Size(min = 3, max = 50)
+    @NotNull(message = "이름은 필수입력 값입니다.")
+    @Pattern(regexp = "^([가-힣]{2,4}|[a-zA-Z]{2,10})\\s?([a-zA-Z]{2,10})?$", message = "한글 이름은 2~4글자, 영어 이름은 앞뒤 각각 2~10글자 작성해야 합니다.")
     private String username;
 
-    @NotNull
-    @Size(min = 3, max = 50)
+    @NotNull(message = "닉네임은 필수입력 값입니다.")
+//    @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,12}$", message = "닉네임은 2~15글자 사이어야 합니다.")
     private String nickname;
 
-    @NotNull
-    @Size(min = 3, max = 50)
+    @NotNull(message = "주소는 필수입력 값입니다.")
+    @Size(max = 50)
     private String address;
 
-    @NotNull
-    @Size(min = 3, max = 50)
+    @NotNull(message = "번호는 필수입력 값입니다.")
+    @Pattern(regexp = "^01[01][0-9]{7,8}$", message = "- 제외하여 입력해야 합니다.")
     private String phone;
 }
