10255c980d55a4b2b6031ddab529f39c3a12871c
kimjaesung <aamoos@naver.com>
2023-10-04 14:05:18 +0900

    배너 조회 querydsl 튜닝

--- src/main/java/com/hellzzang/service/BannerService.java
+++ src/main/java/com/hellzzang/service/BannerService.java
@@ -2,14 +2,16 @@ package com.hellzzang.service;
 
 import com.hellzzang.dto.BannerFileDto;
 import com.hellzzang.dto.QBannerFileDto;
-import com.hellzzang.entity.Banner;
 import com.hellzzang.repository.BannerRepository;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
 import java.util.List;
-import java.util.Optional;
+
 import static com.hellzzang.entity.QBannerFile.bannerFile;
 
 /**
@@ -17,7 +19,7 @@ import static com.hellzzang.entity.QBannerFile.bannerFile;
  * fileName       : BannerService
  * author         : 김재성
  * date           : 2023-05-16
- * description    :
+ * description    : 배너 조회 서비스
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
@@ -29,31 +31,19 @@ import static com.hellzzang.entity.QBannerFile.bannerFile;
 @Transactional
 public class BannerService {
     private final JPAQueryFactory jpaQueryFactory;
-    private final BannerRepository bannerRepository;
 
     @Transactional(readOnly = true)
     public List<BannerFileDto> findBannerFileList(String bannerPath){
 
-        Optional<Banner> optionalBanner = bannerRepository.findByBannerPathAndDelYn(bannerPath, "N");
-        List<BannerFileDto> content = null;
-
-        if(optionalBanner.isEmpty()){
-            optionalBanner = bannerRepository.findByBannerPathAndDelYn("all", "N");
-        }
-
-        if(optionalBanner.isPresent()){
-            Banner banner = optionalBanner.get();
-            content = jpaQueryFactory
-                    .select(new QBannerFileDto(
-                            bannerFile.fileInfo.id
-                            ,bannerFile.fileInfo
-                    ))
-                    .from(bannerFile)
-                    .where(bannerFile.fileInfo.delYn.eq("N"))
-                    .where(bannerFile.banner.id.eq(banner.getId()))
-                    .orderBy(bannerFile.id.desc())
-                    .fetch();
-        }
-        return content;
+        return jpaQueryFactory
+                .select(new QBannerFileDto(
+                         bannerFile.fileInfo.id
+                        ,bannerFile.fileInfo
+                ))
+                .from(bannerFile)
+                .where(bannerFile.banner.bannerPath.in(bannerPath, "all"), bannerFile.banner.delYn.eq("N"))
+                .orderBy(bannerFile.id.desc())
+                .fetch();
     }
+
 }
